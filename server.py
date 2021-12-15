25b606f57b43898eefc34b6deba356ab3295689d
oliviakovacs <kovacs.olivia.s@gmail.com>
2021-12-15 13:11:11 +0100

    Add add_question to server.py add_question handles from.html and data_manager for providing the data dictionary

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import data_manager
 import connection
 
@@ -18,6 +18,23 @@ def display_question(id):
     question = data_manager.get_question_by_id(id)
     answers = data_manager.get_answers_by_question_id(id)
     return render_template("question.html", question=question, answers=answers)
+
+
+@app.route("/add-question", methods=["POST"])
+def add_question():
+    if request.method == "POST":
+        all_questions = connection.get_data(connection.DATA_FILE_PATH_QUESTION)
+        new_question = data_manager.create_new_data(connection.DATA_HEADER_QUESTION, all_questions)
+        for header in connection.DATA_HEADER_QUESTION:
+            if header == "title":
+                new_question.update({header: request.form["title"]})
+            if header == "message":
+                new_question.update({header: request.form["message"]})
+            if header == "image":
+                new_question.update({header: request.form["image"]})
+        return redirect("/list")
+    question_id = None
+    return render_template("form.html", id=question_id)
 
 
 if __name__ == "__main__":
