3e1aea9ed67c0d0add1140741dc09f6a1fe589a9
oliviakovacs <kovacs.olivia.s@gmail.com>
2022-01-27 09:56:30 +0100

    add tries for increase font (needs css)

--- data_manager.py
+++ data_manager.py
@@ -492,30 +492,4 @@ def get_tags(cursor):
     return cursor.fetchall()
 
 
-@connection.connection_handler
-def write_bonus_question(cursor, headers, content):
-    query = """
-    INSERT INTO bonus (%s)
-    VALUES (%s)"""
-    cursor.execute(query, headers, content)
-
-
-# @connection.connection_handler
-# def drop_table(cursor):
-#     query = """
-#     DROP TABLE IF EXISTS public.bonus
-#     """
-#     cursor.execute(query)
-
-
-@connection.connection_handler
-def create_table(cursor, headers):
-    query = """
-    CREATE TABLE bonus
-    (id integer,
-    title text,
-    view_number integer,
-    vote_number integer,
-    description text)"""
-    cursor.execute(query)
 
